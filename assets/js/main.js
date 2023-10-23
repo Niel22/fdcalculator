d2b7ce80e397877ca1d873f1581c6c4839238558
Niel22 <125226257+Niel22@users.noreply.github.com>
2023-10-23 12:36:26 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ function calculateMaturityAmount() {
 
      const maturityAmount = principle * (principle * interestRate * tenure)/100;
 
-     document.getElementById('result').innerText = `Maturity Amount: ${maturityAmount.toFixed(2)}`;
+     document.getElementById('result').innerText = `Maturity Amount: #${maturityAmount.toFixed(2)}`;
 }
 
 document.getElementById('calculateBtn').addEventListener('click', calculateMaturityAmount);
